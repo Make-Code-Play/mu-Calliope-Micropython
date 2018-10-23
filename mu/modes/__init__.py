8e82957f6cfb26c9b7356b96fe8803726db779ec
Moritzheine <44192283+Moritzheine@users.noreply.github.com>
2018-10-23 09:55:59 +0200

    Add files via upload

---
+++ mu/modes/__init__.py
@@ -0,0 +1,9 @@
+from .python3 import PythonMode
+from .adafruit import AdafruitMode
+from .calliope import CalliopeMode
+from .microbit import MicrobitMode
+from .debugger import DebugMode
+from .pygamezero import PyGameZeroMode
+
+__all__ = ['PythonMode', 'AdafruitMode', 'CalliopeMode', 'MicrobitMode', 'DebugMode',
+           'PyGameZeroMode', ]
